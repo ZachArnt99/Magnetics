40ead67621745d8ca0742008d86422c89979e8da
ZachArnt99 <zach_arnt@yahoo.com>
2024-07-07 17:59:49 -0600

    Finished Boost portion of Wk 3 HW

---
+++ Week 3.R
@@ -0,0 +1,19 @@
+lc <- 5 
+muc <- 5
+Ac <- 5
+Rc <- lc/(muc*Ac)
+lg <- 5 #length of the air gap
+mu0 <- 4*pi*10^-7 #H/m
+Rg <- lg/(mu0*Ac)
+i <- 5
+n <- 5
+Phi <- 5
+n <- 5
+i <- 5
+ni <- Phi*Rg
+Bsat <- 5
+Imax <- 5
+Bmax <- 5
+B <- 5
+ni <- B*Ac*Rg
+#n*Imax = Bmax*Ac*Rg=Bmax*lg/mu0
